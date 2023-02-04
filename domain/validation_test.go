473f5fc22b564c57b3f07a288cc1e544a2b5b21c
victorfernandesraton <vfbraton@gmail.com>
2023-02-04 14:06:29 -0300

    test: apply test for minimun password function

--- domain/validation_test.go
+++ domain/validation_test.go
@@ -5,27 +5,49 @@ import (
 	"testing"
 )
 
+func TestSize(t *testing.T) {
+	cases := []struct {
+		desc, text string
+		minimun    int
+		result     bool
+	}{
+		{desc: "string with 3 characters", text: "a@1", minimun: 3, result: true},
+		{desc: "string with 3 characters but test for 2", text: "a@1", minimun: 2, result: true},
+		{desc: "string with 3 characters but test for 4", text: "a@1", minimun: 4, result: false},
+		{desc: "empty string", text: "", minimun: 0, result: true},
+		{desc: "empty string but test for 4", text: "", minimun: 4, result: false},
+	}
+	for _, t1 := range cases {
+		t.Run(t1.desc, func(t *testing.T) {
+			result := domain.MinSize(t1.text, t1.minimun)
+			if result != t1.result {
+				t.Fatalf("expect %v , got %v", t1.result, result)
+			}
+		})
+	}
+}
+
 func TestLowerCase(t *testing.T) {
 
 	cases := []struct {
 		desc, text string
-		expected   int
+		minimun    int
 		result     bool
 		err        error
 	}{
-		{desc: "String with 1 lowercase", text: "oNEPASSWORD_123@AA", expected: 1, result: true, err: nil},
-		{desc: "String with only lowercase", text: "onepassword_123@aa", expected: 13, result: true, err: nil},
-		{desc: "String with 2 lowercases", text: "oNPASSWORD_123@Aa", expected: 2, result: true, err: nil},
-		{desc: "String with 3 lowercases but test with 2", text: "oNpASSWORD_123@Aa", expected: 2, result: true, err: nil},
-		{desc: "String with 1 lowercase but test 2 two", text: "oNEPASSWORD_123@AA", expected: 2, result: false, err: nil},
-		{desc: "String with 0 lowercase", text: "ONEPASSWORD_123@AA", expected: 0, result: true, err: nil},
-		{desc: "empty string", text: "", expected: 0, result: true, err: nil},
-		{desc: "empty string but test with 2", text: "", expected: 0, result: true, err: nil},
+		{desc: "String with 1 lowercase", text: "oNEPASSWORD_123@AA", minimun: 1, result: true, err: nil},
+		{desc: "String with only lowercase", text: "onepassword_123@aa", minimun: 13, result: true, err: nil},
+		{desc: "String with 2 lowercases", text: "oNPASSWORD_123@Aa", minimun: 2, result: true, err: nil},
+		{desc: "String with 3 lowercases but test with 2", text: "oNpASSWORD_123@Aa", minimun: 2, result: true, err: nil},
+		{desc: "String with 1 lowercase but test 2 two", text: "oNEPASSWORD_123@AA", minimun: 2, result: false, err: nil},
+		{desc: "String with 0 lowercase", text: "ONEPASSWORD_123@AA", minimun: 0, result: true, err: nil},
+		{desc: "empty string", text: "", minimun: 0, result: true, err: nil},
+		{desc: "empty string but test with 2", text: "", minimun: 0, result: true, err: nil},
 	}
 
 	for _, t1 := range cases {
 		t.Run(t1.desc, func(t *testing.T) {
-			result, err := domain.MinLowerCase(t1.text, t1.expected)
+			result, err := domain.MinLowerCase(t1.text, t1.minimun)
 			if err != t1.err {
 				t.Fatalf("expect %v , got %v", t1.err, err)
 			}
@@ -40,23 +62,23 @@ func TestUpperCase(t *testing.T) {
 
 	cases := []struct {
 		desc, text string
-		expected   int
+		minimun    int
 		result     bool
 		err        error
 	}{
-		{desc: "String with 1 uppercase", text: "one_Password_123@aa", expected: 1, result: true, err: nil},
-		{desc: "String with only uppercase", text: "ONEPASSWORD_123@AA", expected: 13, result: true, err: nil},
-		{desc: "String with 2 uppercase", text: "onPassword_123@aA", expected: 2, result: true, err: nil},
-		{desc: "String with 3 uppercase but test with 2", text: "OnPassword_123@aA", expected: 2, result: true, err: nil},
-		{desc: "String with 1 uppercase but test 2 two", text: "one_Password_123@aa", expected: 2, result: false, err: nil},
-		{desc: "String with 0 uppercase", text: "onepassword_123@aa", expected: 0, result: true, err: nil},
-		{desc: "empty string", text: "", expected: 0, result: true, err: nil},
-		{desc: "empty string but test with 2", text: "", expected: 0, result: true, err: nil},
+		{desc: "String with 1 uppercase", text: "one_Password_123@aa", minimun: 1, result: true, err: nil},
+		{desc: "String with only uppercase", text: "ONEPASSWORD_123@AA", minimun: 13, result: true, err: nil},
+		{desc: "String with 2 uppercase", text: "onPassword_123@aA", minimun: 2, result: true, err: nil},
+		{desc: "String with 3 uppercase but test with 2", text: "OnPassword_123@aA", minimun: 2, result: true, err: nil},
+		{desc: "String with 1 uppercase but test 2 two", text: "one_Password_123@aa", minimun: 2, result: false, err: nil},
+		{desc: "String with 0 uppercase", text: "onepassword_123@aa", minimun: 0, result: true, err: nil},
+		{desc: "empty string", text: "", minimun: 0, result: true, err: nil},
+		{desc: "empty string but test with 2", text: "", minimun: 0, result: true, err: nil},
 	}
 
 	for _, t1 := range cases {
 		t.Run(t1.desc, func(t *testing.T) {
-			result, err := domain.MinUpperCase(t1.text, t1.expected)
+			result, err := domain.MinUpperCase(t1.text, t1.minimun)
 			if err != t1.err {
 				t.Fatalf("expect %v , got %v", t1.err, err)
 			}
