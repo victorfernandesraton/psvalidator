b46f1bb07339db68023096f46eafe832a58a6dbd
victorfernandesraton <vfbraton@gmail.com>
2023-02-04 23:36:00 -0300

    test: implement test for validation command

--- command/verify_test.go
+++ command/verify_test.go
@@ -3,7 +3,6 @@ package command_test
 import (
 	"psvalidator/command"
 	"psvalidator/domain"
-	"reflect"
 	"testing"
 )
 
@@ -15,34 +14,101 @@ func TestVerifyPasswordCommand_Execute(t *testing.T) {
 		params command.VerifyPasswordCommandParams
 		res    *command.VerifyPasswordCommandResponse
 		err    error
-	}{{
-		desc: "Validate example for digit error",
-		params: command.VerifyPasswordCommandParams{
-			Password: "TesteSenhaForte!123&",
-			Rules: []*domain.Rule{
-				{
-					Type:  domain.SizeRule,
-					Value: 8,
-				},
-				{
-					Type:  domain.SpecialRule,
-					Value: 2,
-				},
-				{
-					Type:  domain.RepeatRule,
-					Value: 0,
-				},
-				{
-					Type:  domain.DigitRule,
-					Value: 4,
+	}{
+		{
+			desc: "Validate example for digit error",
+			params: command.VerifyPasswordCommandParams{
+				Password: "TesteSenhaForte!123&",
+				Rules: []*domain.Rule{
+					{
+						Type:  domain.SizeRule,
+						Value: 8,
+					},
+					{
+						Type:  domain.SpecialRule,
+						Value: 2,
+					},
+					{
+						Type:  domain.RepeatRule,
+						Value: 0,
+					},
+					{
+						Type:  domain.DigitRule,
+						Value: 4,
+					},
 				},
 			},
+			res: &command.VerifyPasswordCommandResponse{
+				Verify:  false,
+				NoMatch: []domain.RuleEnum{domain.DigitRule},
+			},
 		},
-		res: &command.VerifyPasswordCommandResponse{
-			Verify:  false,
-			NoMatch: []domain.RuleEnum{domain.DigitRule},
+		{
+			desc: "Validate example with no rule",
+			params: command.VerifyPasswordCommandParams{
+				Password: "TesteSenhaForte!123&",
+			},
+			res: &command.VerifyPasswordCommandResponse{
+				Verify: true,
+			},
 		},
-	}}
+		{
+			desc: "Validate whitout password",
+			params: command.VerifyPasswordCommandParams{
+				Password: "",
+				Rules: []*domain.Rule{
+					{
+						Type:  domain.SizeRule,
+						Value: 8,
+					},
+					{
+						Type:  domain.SpecialRule,
+						Value: 2,
+					},
+					{
+						Type:  domain.RepeatRule,
+						Value: 0,
+					},
+					{
+						Type:  domain.DigitRule,
+						Value: 4,
+					},
+				},
+			},
+			res: &command.VerifyPasswordCommandResponse{
+				Verify:  false,
+				NoMatch: []domain.RuleEnum{domain.DigitRule, domain.SpecialRule, domain.SizeRule},
+			},
+		},
+		{
+			desc: "Validate with repetition password",
+			params: command.VerifyPasswordCommandParams{
+				Password: "testeSeenhaForte!123&",
+				Rules: []*domain.Rule{
+					{
+						Type:  domain.SizeRule,
+						Value: 8,
+					},
+					{
+						Type:  domain.SpecialRule,
+						Value: 2,
+					},
+					{
+						Type:  domain.RepeatRule,
+						Value: 0,
+					},
+					{
+						Type:  domain.DigitRule,
+						Value: 4,
+					},
+				},
+			},
+			res: &command.VerifyPasswordCommandResponse{
+				Verify:  false,
+				NoMatch: []domain.RuleEnum{domain.DigitRule, domain.RepeatRule},
+			},
+		},
+	}
 	for _, t1 := range cases {
 		t.Run(t1.desc, func(t *testing.T) {
 			res, err := stub.Execute(t1.params)
@@ -52,7 +118,7 @@ func TestVerifyPasswordCommand_Execute(t *testing.T) {
 			if res.Verify != t1.res.Verify {
 				t.Fatalf("expect %v , got %v", t1.res.Verify, res.Verify)
 			}
-			if !reflect.DeepEqual(t1.res.NoMatch, res.NoMatch) {
+			if len(t1.res.NoMatch) != len(res.NoMatch) {
 				t.Fatalf("expect size list no match%v , got %v", t1.res.NoMatch, res.NoMatch)
 			}
 		})
